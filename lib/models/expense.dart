221a592778733dea195d0127fe153c7e7b7e2315
naveen kumar <7naveennn@gmail.com>
2024-06-10 16:11:46 +0530

    Added charts

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -32,3 +32,23 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  //additional cunstructor
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+  double get totalExpenses {
+    double total = 0;
+    for (final expense in expenses) {
+      total += expense.amount;
+    }
+    return total;
+  }
+}
