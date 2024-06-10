a49c84f816ef9fc1d829453783464c68f7d18e09
naveen kumar <7naveennn@gmail.com>
2024-06-10 12:39:23 +0530

    Added Datepicker value and Category Dropdown

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,8 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,6 +15,8 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.work;
 
   @override
   void dispose() {
@@ -20,16 +26,19 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
 
-    Future date = showDatePicker(
+    final date = await showDatePicker(
       context: context,
       initialDate: now,
       firstDate: firstDate,
       lastDate: now,
     );
+    setState(() {
+      _selectedDate = date;
+    });
   }
 
   @override
@@ -38,6 +47,7 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(10.0),
       child: Column(
         children: [
+          //TextField for title
           TextField(
             controller: _titleController,
             maxLength: 30,
@@ -45,6 +55,9 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Expense name'),
             ),
           ),
+          const SizedBox(height: 16),
+
+          //Row for amount and date
           Row(
             children: [
               Expanded(
@@ -63,7 +76,9 @@ class _NewExpenseState extends State<NewExpense> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected Date'),
+                    Text(_selectedDate == null
+                        ? 'No Date Chosen'
+                        : formatter.format(_selectedDate!)),
                     IconButton(
                       onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month),
@@ -73,8 +88,30 @@ class _NewExpenseState extends State<NewExpense> {
               )
             ],
           ),
+          const SizedBox(height: 16),
+          //Row for dropdown and save button
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values.map((category) {
+                  return DropdownMenuItem(
+                    value: category,
+                    child: Text(
+                      category.name.toString().toUpperCase(),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              Spacer(),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
